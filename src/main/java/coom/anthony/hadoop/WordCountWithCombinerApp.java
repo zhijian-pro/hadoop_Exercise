29499a5dd999d043367619b17fb4c00c01dc2efc
段渣渣 <1581041541@qq.com>
2018-03-25 12:53:44 +0800

    添加删除已经存在文件夹，combiner和partition功能

---
+++ src/main/java/coom/anthony/hadoop/WordCountWithCombinerApp.java
@@ -0,0 +1,105 @@
+package coom.anthony.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * @Description: 使用MapReduce开发Wordcount程序 添加自动清理已经存在目录方法
+ * @Date: Created in 10:36 2018/3/25
+ * @Author: Anthony_Duan
+ */
+public class WordCountWithCombinerApp {
+
+    /**
+     * map:读取输入的文件
+     */
+    public static class MyMapper extends Mapper<LongWritable,Text,Text,LongWritable>{
+
+        LongWritable one  = new LongWritable(1);
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+//            接受到的每一行数据
+            String line = value.toString();
+
+//            按照指定分隔符进行拆分
+            String words[] = line.split(" ");
+
+            for (String word: words) {
+//                通过上下文把map结果输出
+                context.write(new Text(word),one);
+            }
+        }
+    }
+
+
+    /**
+     * reducer:归并操作
+     */
+    public static class MyReducer extends Reducer<Text,LongWritable,Text,LongWritable>{
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+           long sum = 0;
+            for (LongWritable value:values) {
+                sum += value.get();
+            }
+//          最终结果输出
+            context.write(key,new LongWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+//        创建Configuration
+        Configuration configuration = new Configuration();
+
+
+//        准备清理已经存在的输出目录
+        Path outputPath = new Path(args[1]);
+
+        FileSystem fileSystem = FileSystem.get(configuration);
+        if (fileSystem.exists(outputPath)){
+            fileSystem.delete(outputPath,true);
+            System.out.println("output file exists,but is has deleted");
+        }
+
+//        创建job   抛出异常快捷键 alt+enter
+        Job job = Job.getInstance(configuration,"wordcount");
+
+//        设置job的处理类
+        job.setJarByClass(WordCountWithCombinerApp.class);
+//        设置作业的输入路径
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+
+//         设置map相关参数
+        job.setMapperClass(MyMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+
+//        通过job设置combiner处理类 逻辑上和reduce一样
+        job.setCombinerClass(MyReducer.class);
+
+
+//        设置reduce相关参数
+        job.setReducerClass(MyReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+//        设置作业的输出路径
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1 );
+
+
+    }
+}
